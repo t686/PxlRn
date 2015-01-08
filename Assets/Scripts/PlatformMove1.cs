7225726679633a2aa89ab6fcd6a08178608aaddd
adr <andrew.dremov@gmail.com>
2015-01-09 01:02:30 +0200

    Revert "Revert "DestoyWall script added""
    This reverts commit cab05fb0c504724d319c9d521cdfcce8a6e601d1.

---
+++ Assets/Scripts/PlatformMove1.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMove : MonoBehaviour {
+
+	private float Xpos;
+	private float Ypos;
+	private bool max;
+
+	public bool Vert;
+	public int maxAmount;
+	public float step;
+
+	// Use this for initialization
+	void Start () {
+		Xpos = transform.position.x;
+		Ypos = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Set Max
+		if(Vert) { //Vertical
+			if(transform.position.y >= Ypos+maxAmount)
+				max = true;
+			else if(transform.position.y <= Ypos)
+				max = false;
+		} else { //Horizontal
+			if(transform.position.x >= Xpos+maxAmount)
+				max = true;
+			else if(transform.position.x <= Xpos)
+				max = false;
+		}
+
+		//Moving the Platform
+		if(Vert) {//Vertical movement
+			if(!max) 
+				transform.position.y += step;
+			else 
+				transform.position.y -= step;
+		} else {
+			if(!max)
+				transform.position.x += step;
+			else
+				transform.position.x -= step;
+		}
+	}
+}
---
+++ Assets/Scripts/wallDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallDestroy : MonoBehaviour {
+
+
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag == "Player"){
+			Destroy(gameObject, 2.0f);
+		}    
+	}
+}
